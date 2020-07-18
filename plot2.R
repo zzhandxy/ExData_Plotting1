79f40e73637fd3f84ea5b9a93c83f06812dca984
zzhandxy <zzhandxy@buaa.edu.cn>
2020-07-18 18:47:15 +0800

    R script for the second figure

---
+++ plot2.R
@@ -0,0 +1,30 @@
+### load the packages used in this assignment
+Sys.setlocale("LC_TIME", "English")
+library(dplyr)
+library(lubridate)
+
+### load the original data  
+
+ori_data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+
+### data processing
+
+ori_data$Date <- dmy(ori_data$Date)
+date <- c(ymd("2007-02-01"), ymd("2007-02-02"))
+data <- filter(ori_data, ori_data$Date %in% date)
+data <- mutate(data, date_time = paste(as.character(data$Date), data$Time))
+data$date_time <- ymd_hms(data$date_time)
+data <- data[,-c(1,2)]
+start <- data$date_time[1]
+end <- data$date_time[nrow(data)]
+diff <- as.numeric(difftime(end, start, units= "mins")) 
+
+### plot the figure "plot2.png"
+
+png(file = "plot2.png", width = 480, height = 480)
+x <- 0:diff
+plot(x, data$Global_active_power, type = "n", xaxt="n", xlab = " ", ylab = "Global Acitive Power (kilowatts)")
+lines(x, data$Global_active_power, axis=FALSE)
+axis(1, at=c(0,round(0.5*length(x)),length(x)),labels=c("Thu", "Fri", "Sat"))
+dev.off()
+
